c523f3f91c7703dbd64972f5769dfa8c8bbebeec
Georgy Kozlov <kgk2409@gmail.com>
2018-01-13 18:34:10 +0400

    Now header changes, when user log in/log out

--- src/app/modals/login/login.component.ts
+++ src/app/modals/login/login.component.ts
@@ -1,97 +1,94 @@
-    ///<reference path="../../constants/api.constant.ts"/>
-    import { Component } from '@angular/core';
-    import { MatDialogRef } from "@angular/material";
-    import { FormControl, FormGroup, Validators } from "@angular/forms";
-    import { SIGN_IN, SIGN_UP } from "../../constants/api.constant";
-    import { HttpClient } from "@angular/common/http";
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { SIGN_IN, SIGN_UP, USER_INFO } from "../../constants/api.constant";
+import { HttpClient } from "@angular/common/http";
+import { UserService } from "../../services/user.service";
+import { MatDialog } from "@angular/material";
 
-    @Component({
-        selector: 'login-modal',
-        templateUrl: './login.component.html',
-        styleUrls: ['./login.component.less']
-    })
-    export class LoginModalComponent {
+@Component({
+    selector: 'login-modal',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.less']
+})
 
-        private confirmed: boolean = false;
+export class LoginModalComponent {
 
-        public type: string = 'entry';
+    public type: string = 'entry';
 
-        registerForm: FormGroup;
-        loginForm: FormGroup;
+    registerForm: FormGroup;
+    loginForm: FormGroup;
 
-        constructor(private dialogRef: MatDialogRef<any>,
-                    private http: HttpClient) {
+    constructor(private dialog: MatDialog,
+                private http: HttpClient,
+                private userService: UserService) {
 
-            this.registerForm = new FormGroup({
+        this.loginForm = new FormGroup({
 
-                "typeAccount": new FormControl('aspirant', [
-                    Validators.required,
-                ]),
+            "email": new FormControl('', [
+                Validators.required,
+            ]),
 
-                "email": new FormControl('',[
-                    Validators.required,
-                    Validators.email
-                ]),
+            "password": new FormControl('', [
+                Validators.required,
+            ]),
 
-                "password": new FormControl('', [
-                    Validators.required,
-                    this.passwordValidator,
-                ]),
+        });
+        this.registerForm = new FormGroup({
 
-                "confirmPassword": new FormControl('', [
-                    Validators.required,
-                ]),
+            "typeAccount": new FormControl('aspirant', [
+                Validators.required,
+            ]),
 
-            });
+            "email": new FormControl('', [
+                Validators.required,
+                Validators.email
+            ]),
 
-            this.loginForm = new FormGroup({
+            "password": new FormControl('', [
+                Validators.required,
+                this.passwordValidator,
+            ]),
 
-                "email": new FormControl('',[
-                    Validators.required,
-                ]),
+            "confirmPassword": new FormControl('', [
+                Validators.required,
+            ]),
 
-                "password": new FormControl('', [
-                    Validators.required,
-                ]),
-
-            });
-        }
-
-
-        passwordValidator(control: FormControl ) : {[s:string]:boolean} {
-            if (control.value.length < 8) {
-                return {"password": true};
-            }
-            return null;
-        }
-
-        public login(): void {
-            this.http.post(SIGN_IN, this.loginForm.value)
-                .subscribe((res: any) => {
-                    // this.confirmed = res.code === 200;
-
-                    if (res.success === true) {
-                        alert('Вы вошли')
-                    } else {
-                        alert(res.errorMessage);
-                        console.log(res);
-                    }
-
-                })
-        }
-
-        public register(): void {
-            this.http.post(SIGN_UP, this.registerForm.value)
-                .subscribe((res: any) => {
-                    // this.confirmed = res.code === 200;
-
-                    if (res.success === true) {
-                        alert('Вы зарегестрированы!')
-                    } else {
-                        alert(res.errorMessage);
-                        console.log(res)
-                    }
-
-                })
-        }
+        });
     }
+
+
+    passwordValidator(control: FormControl): { [s: string]: boolean } {
+        if (control.value.length < 8) {
+            return { "password": true };
+        }
+        return null;
+    }
+
+    public login(): void {
+        this.http.post(SIGN_IN, this.loginForm.value)
+            .subscribe((res: any) => {
+                if (res.success === true) {
+                    alert('Вы вошли');
+                    this.userService.getUserInfo();
+                    this.dialog.closeAll();
+                } else {
+                    alert(res.errorMessage);
+                }
+            });
+    }
+
+    public register(): void {
+        this.http.post(SIGN_UP, this.registerForm.value)
+            .subscribe((res: any) => {
+                // this.confirmed = res.code === 200;
+
+                if (res.success === true) {
+                    alert('Вы зарегестрированы!');
+                } else {
+                    alert(res.errorMessage);
+                    console.log(res);
+                }
+
+            })
+    }
+}
