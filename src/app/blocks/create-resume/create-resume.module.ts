fd94821d5e7b7ac167f5496c178b6c1143f3f54b
vovcyan <vovcyan@yandex.ru>
2018-01-28 15:11:52 +0400

    Add date of birth and custom select

--- src/app/blocks/create-resume/create-resume.module.ts
+++ src/app/blocks/create-resume/create-resume.module.ts
@@ -1,16 +1,29 @@
-import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { CreateResumeComponent } from "./create-resume.component";
+import { NgModule } from "@angular/core";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import {
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatOptionModule,
+    MatSelectModule
+} from "@angular/material";
 
+import { CreateResumeComponent } from "./create-resume.component";
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatOptionModule
     ],
     declarations: [CreateResumeComponent],
     exports: [CreateResumeComponent]
 })
-export class CreateResumeModule {}
+export class CreateResumeModule {
+}
