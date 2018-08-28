c5a210e645a0aa692ba1554c73695b5b9a776bba
Kozlov Georgiy <kgk2409@gmail.com>
2018-08-28 13:48:07 +0400

    Добавлен перевод на дейт пикер

--- src/app/modals/birthday/birthday.component.ts
+++ src/app/modals/birthday/birthday.component.ts
@@ -1,12 +1,25 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
 import * as moment from 'moment';
-import {Moment} from 'moment';
-import {LocalizationService} from '../../services/localization.service';
+import { Moment } from 'moment';
+import { LocalizationService } from '../../services/localization.service';
 
 @Component({
     selector: 'birthday-date-picker',
     templateUrl: './birthday.component.html',
-    styleUrls: ['./birthday.component.less']
+    styleUrls: ['./birthday.component.less'],
+    providers: [
+        // The locale would typically be provided on the root module of your application. We do it at
+        // the component level here, due to limitations of our example generation script.
+        { provide: MAT_DATE_LOCALE, useValue: 'en-EN' },
+
+        // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
+        // `MatMomentDateModule` in your applications root module. We provide it at the component level
+        // here, due to limitations of our example generation script.
+        { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+    ],
 })
 export class BirthdayComponent implements OnInit {
     @Output() changed: EventEmitter<any> = new EventEmitter();
@@ -15,8 +28,8 @@ export class BirthdayComponent implements OnInit {
     public data: string = null;
     public dictionary: any = null;
 
-    constructor(private _localizationService: LocalizationService,) {
-
+    constructor(private _localizationService: LocalizationService,
+                private adapter: DateAdapter<any>) {
     }
 
     public ngOnInit() {
@@ -24,6 +37,12 @@ export class BirthdayComponent implements OnInit {
         this.data = this.dictionary.CHANGE_BIRTHDAY;
         if (this.dataInput) {
             this.dateChanged(moment(this.dataInput));
+        }
+
+        if (LocalizationService.currentLang() === 'ru') {
+            this.adapter.setLocale('ru');
+        } else {
+            this.adapter.setLocale('en');
         }
     }
 
@@ -34,5 +53,5 @@ export class BirthdayComponent implements OnInit {
         const prefix: string = (diffYears > 4) ? 'лет' : (diffYears === 1) ? 'год' : 'года';
         this.data = `${formattedDate} (${diffYears}) ${prefix}`;
         this.changed.emit(date);
-    };
+    }
 }
