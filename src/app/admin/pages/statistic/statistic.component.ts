4b22342b78d186a066664ab4067e0c7be9ec3a3c
Kozlov Georgiy <kgk2409@gmail.com>
2019-01-26 01:15:55 +0400

    общий график 3 and 4

--- src/app/admin/pages/statistic/statistic.component.ts
+++ src/app/admin/pages/statistic/statistic.component.ts
@@ -12,11 +12,43 @@ export class StatisticComponent implements OnInit {
   statCommonResumeViewBuyPerDay = [];
   statCommonNewRespondsPerDay = [];
 
+  public attitudeRespondsTemplate = [
+    { name: 'Ожидание', value: 0, active: true },
+    { name: 'Ожидание опросника', value: 0, active: true },
+    { name: 'Опросник заполнен', value: 0, active: true },
+    { name: 'Принято', value: 0, active: true },
+    { name: 'Отклонено', value: 0, active: true },
+  ];
+  public attitudeResponds = [
+    { name: 'Ожидание', value: 1 },
+    { name: 'Ожидание опросника', value: 2 },
+    { name: 'Опросник заполнен', value: 3 },
+    { name: 'Принято', value: 4 },
+    { name: 'Отклонено', value: 5 },
+  ];
+
+  public attitudeOffersTemplate = [
+    { name: 'Ожидание', value: 0, active: true },
+    { name: 'Ожидание опросника', value: 0, active: true },
+    { name: 'Опросник заполнен', value: 0, active: true },
+    { name: 'Принято', value: 0, active: true },
+    { name: 'Отклонено', value: 0, active: true },
+  ];
+  public attitudeOffers = [
+    { name: 'Ожидание', value: 1 },
+    { name: 'Ожидание опросника', value: 2 },
+    { name: 'Опросник заполнен', value: 3 },
+    { name: 'Принято', value: 4 },
+    { name: 'Отклонено', value: 5 },
+  ];
+
   constructor(private analyticsApi: AnalyticsApi) {}
 
   ngOnInit() {
     this.getStatCommonResumeViewBuyPerDay();
     this.getStatCommonNewRespondsPerDay();
+    this.getAttitudeResponds();
+    this.getAttitudeOffers();
   }
 
   getStatCommonResumeViewBuyPerDay() {
@@ -46,6 +78,54 @@ export class StatisticComponent implements OnInit {
           };
         });
       });
+  }
+
+  getAttitudeResponds() {
+    this.analyticsApi.statCommonAttitudeRespondsStatusNow()
+      .subscribe(
+        res => {
+          this.attitudeRespondsTemplate[0].value = res.statistics.AWAITING || 0;
+          this.attitudeRespondsTemplate[1].value = res.statistics.WAIT_QUESTIONNAIRE || 0;
+          this.attitudeRespondsTemplate[2].value = res.statistics.QUESTIONNAIRE_DONE || 0;
+          this.attitudeRespondsTemplate[3].value = res.statistics.APPROVED || 0;
+          this.attitudeRespondsTemplate[4].value = res.statistics.REJECTED || 0;
+          this.updateAttitudeResponds();
+        },
+      );
+  }
+
+  updateAttitudeResponds() {
+    this.attitudeResponds = [
+      { name: 'Ожидание', value: this.attitudeRespondsTemplate[0].active ? this.attitudeRespondsTemplate[0].value : 0 },
+      { name: 'Ожидание опросника', value: this.attitudeRespondsTemplate[1].active ? this.attitudeRespondsTemplate[1].value : 0 },
+      { name: 'Опросник заполнен', value: this.attitudeRespondsTemplate[2].active ? this.attitudeRespondsTemplate[2].value : 0 },
+      { name: 'Принято', value: this.attitudeRespondsTemplate[3].active ? this.attitudeRespondsTemplate[3].value : 0 },
+      { name: 'Отклонено', value: this.attitudeRespondsTemplate[4].active ? this.attitudeRespondsTemplate[4].value : 0 },
+    ];
+  }
+
+  getAttitudeOffers() {
+    this.analyticsApi.statCommonAttitudeOffersStatusNow()
+      .subscribe(
+        res => {
+          this.attitudeOffersTemplate[0].value = res.statistics.AWAITING || 0;
+          this.attitudeOffersTemplate[1].value = res.statistics.WAIT_QUESTIONNAIRE || 0;
+          this.attitudeOffersTemplate[2].value = res.statistics.QUESTIONNAIRE_DONE || 0;
+          this.attitudeOffersTemplate[3].value = res.statistics.APPROVED || 0;
+          this.attitudeOffersTemplate[4].value = res.statistics.REJECTED || 0;
+          this.updateAttitudeOffers();
+        },
+      );
+  }
+
+  updateAttitudeOffers() {
+    this.attitudeOffers = [
+      { name: 'Ожидание', value: this.attitudeOffersTemplate[0].active ? this.attitudeOffersTemplate[0].value : 0 },
+      { name: 'Ожидание опросника', value: this.attitudeOffersTemplate[1].active ? this.attitudeOffersTemplate[1].value : 0 },
+      { name: 'Опросник заполнен', value: this.attitudeOffersTemplate[2].active ? this.attitudeOffersTemplate[2].value : 0 },
+      { name: 'Принято', value: this.attitudeOffersTemplate[3].active ? this.attitudeOffersTemplate[3].value : 0 },
+      { name: 'Отклонено', value: this.attitudeOffersTemplate[4].active ? this.attitudeOffersTemplate[4].value : 0 },
+    ];
   }
 
 }
