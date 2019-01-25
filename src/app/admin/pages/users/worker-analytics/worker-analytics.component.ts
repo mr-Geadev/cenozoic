82662a24d1ac63b4b8e8cf537cf8e38fecd59cd4
Kozlov Georgiy <kgk2409@gmail.com>
2019-01-26 00:25:26 +0400

    Статистика по воркеру

--- src/app/admin/pages/users/worker-analytics/worker-analytics.component.ts
+++ src/app/admin/pages/users/worker-analytics/worker-analytics.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, OnChanges, OnInit, Output, ViewEncapsulation } from '@angular/core';
 import { MatDialog } from '@angular/material';
 import { AnalyticsApi } from 'api';
+import * as moment from 'moment';
 import { ConfirmService } from '../../../../services/confirm.service';
 import { UserModel } from '../../../../models/user.model';
 import { SystemMessageService, UserService } from '../../../../services';
@@ -16,9 +17,32 @@ export class WorkerAnalyticsComponent implements OnInit, OnChanges {
 
   @Input() user: UserModel;
 
+  public statWorkerViewVacancyPerDay: any[] = [];
+
+  constructor(public analyticsApi: AnalyticsApi) {
+  }
+
   public ngOnInit() {}
 
-  public ngOnChanges() {}
+  public ngOnChanges() {
+    this.getStatWorkerViewVacancyPerDay();
+  }
+
+  getStatWorkerViewVacancyPerDay() {
+    this.analyticsApi.statWorkerViewVacancyPerDay(this.user._id)
+      .subscribe(
+        res => {
+          this.statWorkerViewVacancyPerDay = res.statistics.map(day => {
+            return {
+              name: moment(day.date).format('YY.MM.YY'),
+              series: [
+                { name: 'Просмотренно', value: day.count || 0 },
+              ],
+            };
+          });
+        },
+      );
+  }
 }
 
 interface Status {
