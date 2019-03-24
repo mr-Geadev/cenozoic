a9ac2c2f911aa04fc8cbefdf6306bc265a5f722f
Kozlov Georgiy <kgk2409@gmail.com>
2019-03-25 01:36:34 +0400

    добавил в админку фильтр для баннеров

--- src/app/admin/pages/banner/banner.component.ts
+++ src/app/admin/pages/banner/banner.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { BannerApi } from 'api';
 import { BannerModel } from 'models';
+import { map, tap } from 'rxjs/operators';
 import { LocalizationService } from 'services';
 
 @Component({
@@ -11,6 +13,27 @@ import { LocalizationService } from 'services';
 export class BannerComponent implements OnInit {
 
   public listBanners: BannerModel[] = [];
+  public defaultListBanners: BannerModel[] = [];
+
+  public filter = new FormControl('');
+
+  public filter$ = this.filter.valueChanges.pipe(
+    tap((val) => {
+      if (val === 'publicated') {
+        this.listBanners = this.defaultListBanners.filter(banner => banner.status === 0 && banner.publicate);
+        return;
+      }
+      if (val === 'timeouted') {
+        this.listBanners = this.defaultListBanners.filter(banner => banner.status === 3);
+        return;
+      }
+      if (val === 'in_view') {
+        this.listBanners = this.defaultListBanners.filter(banner => banner.status === 0 && !banner.publicate );
+        return;
+      }
+      this.listBanners = this.defaultListBanners.slice();
+    })
+  );
 
   private currentLang: string = null;
 
@@ -20,10 +43,16 @@ export class BannerComponent implements OnInit {
   ngOnInit() {
     this.bannerApi.getListAdminBanner()
       .subscribe(
-        res => { res['banners'].map((banner) => this.listBanners.push(new BannerModel(banner))); },
+        res => {
+          res['banners'].map((banner) => {
+            this.defaultListBanners.push(new BannerModel(banner));
+            this.listBanners.push(new BannerModel(banner));
+          });
+        }
       );
 
     this.currentLang = LocalizationService.currentLang();
+    this.filter$.subscribe();
   }
 
 }
