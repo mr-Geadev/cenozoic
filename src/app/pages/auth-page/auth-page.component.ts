39c7c8ec5e6452d221325803aea9aa660f481a76
Kozlov Georgiy <kgk2409@gmail.com>
2019-04-13 20:23:41 +0400

    Обновления по странице авторизации

--- src/app/pages/auth-page/auth-page.component.ts
+++ src/app/pages/auth-page/auth-page.component.ts
@@ -1,79 +1,90 @@
-import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {MatDialog} from '@angular/material';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatDialog } from '@angular/material';
+import { RestorePasswordService } from 'pop-ups/restore-password';
 
-import {AuthService, LocalizationService, SystemMessageService, UserService} from '../../services';
-import {Router} from '@angular/router';
+import { AuthService, LocalizationService, SystemMessageService, UserService } from '../../services';
+import { Router } from '@angular/router';
 
 @Component({
-    selector: 'auth-page',
-    templateUrl: './auth-page.component.html',
-    styleUrls: ['./auth-page.component.scss']
+  selector: 'auth-page',
+  templateUrl: './auth-page.component.html',
+  styleUrls: ['./auth-page.component.scss'],
 })
 
 export class AuthPageComponent implements OnInit {
 
-    public type: string = 'entry';
-    public dictionary: any = {};
+  public type: string = 'entry';
+  public dictionary: any = {};
+  public currentLang: string = '';
+  public privacyPoliticAccepted: boolean = false;
 
-    public registerForm: FormGroup = new FormGroup({
-        typeAccount: new FormControl('worker', Validators.required),
-        email: new FormControl(null, [Validators.required, Validators.email]),
-        password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
-        confirmPassword: new FormControl(null, Validators.required)
-    });
+  public registerForm: FormGroup = new FormGroup({
+    typeAccount: new FormControl('worker', Validators.required),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(8)]),
+    confirmPassword: new FormControl(null, Validators.required),
+  });
 
-    public loginForm: FormGroup = new FormGroup({
-        email: new FormControl(null, Validators.required),
-        password: new FormControl(null, Validators.required),
-    });
+  public loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
+  });
 
-    constructor(private _dialog: MatDialog,
-                private _systemMessageService: SystemMessageService,
-                private _localizationService: LocalizationService,
-                private _router: Router,
-                private _userService: UserService,
-                private _authService: AuthService) {
-    }
+  constructor(private _dialog: MatDialog,
+              private _systemMessageService: SystemMessageService,
+              private _localizationService: LocalizationService,
+              private restorePasswordService: RestorePasswordService,
+              private _router: Router,
+              private _userService: UserService,
+              private _authService: AuthService) {
+  }
 
-    ngOnInit(): void {
-      this._localizationService.currentDictionary
-        .subscribe(
-          res => this.dictionary = res
-        );
-    }
+  ngOnInit(): void {
+    this._localizationService.currentDictionary
+      .subscribe(
+        res => {
+          this.dictionary = res;
+          this.currentLang = LocalizationService.currentLang();
+        }
+      );
+  }
 
-    public logIn(): void {
-        this._authService.loginUser(this.loginForm.value)
-            .first()
-            .subscribe(
-                (res) => {
-                    this._userService.getUserInfo();
-                    this._systemMessageService.info(this.dictionary.INFO_MESSAGES_SUCCESS_LOG_IN);
-                    this._router.navigate(['/']);
-                    this._dialog.closeAll();
-                },
-                (err) => {
-                    this._systemMessageService.info(err.error.errorMessage);
-                }
-            );
-    };
+  public logIn(): void {
+    this._authService.loginUser(this.loginForm.value)
+      .first()
+      .subscribe(
+        (res) => {
+          this._userService.getUserInfo();
+          this._systemMessageService.info(this.dictionary.INFO_MESSAGES_SUCCESS_LOG_IN);
+          this._router.navigate(['/']);
+          this._dialog.closeAll();
+        },
+        (err) => {
+          this._systemMessageService.info(err.error.errorMessage);
+        },
+      );
+  };
 
-    public signUp(): void {
-        this._authService.registerUser(this.registerForm.value)
-            .first()
-            .subscribe(
-                (res) => {
-                    this._authService.loginUser(this.registerForm.value)
-                        .subscribe((res) => {
-                            this._userService.getUserInfo();
-                        });
-                    this._systemMessageService.info(this.dictionary.INFO_MESSAGES_SUCCESS_LOG_UP);
-                    this._dialog.closeAll();
-                },
-                (err) => {
-                    this._systemMessageService.info(err.error.errorMessage);
-                });
+  public signUp(): void {
+    this._authService.registerUser(this.registerForm.value)
+      .first()
+      .subscribe(
+        (res) => {
+          this._authService.loginUser(this.registerForm.value)
+            .subscribe((res) => {
+              this._userService.getUserInfo();
+            });
+          this._systemMessageService.info(this.dictionary.INFO_MESSAGES_SUCCESS_LOG_UP);
+          this._dialog.closeAll();
+        },
+        (err) => {
+          this._systemMessageService.info(err.error.errorMessage);
+        });
 
-    }
+  }
+
+  public restorePassword() {
+    this.restorePasswordService.restorePassword();
+  }
 }
